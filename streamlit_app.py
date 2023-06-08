fce639645ae51c284619f5a1bfb33b8f4a8567a6
Blaze28 <78606303+blaze28@users.noreply.github.com>
2023-06-08 07:49:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,28 @@
 st.sidebar.title("Your Information")
 
-   Name = st.sidebar.text_input("Full Name")
-   Contact_Number = st.sidebar.text_input("Contact Number")
-   Email_address = st.sidebar.text_input("Email address")
+Name = st.sidebar.text_input("Full Name")
+Contact_Number = st.sidebar.text_input("Contact Number")
+Email_address = st.sidebar.text_input("Email address")
 
-    if not Name or not Email_address:
-        st.sidebar.warning("Please fill out your name and email address")
-    else:
-        if st.sidebar.button("Submit"):
+if not Name or not Email_address:
+   st.sidebar.warning("Please fill out your name and email address")
+else:
+   if st.sidebar.button("Submit"):
         # Define the path to store the entered data
-            csv_path = "data.csv"
+       csv_path = "data.csv"
 
         # Prepare the data as a dictionary
-            data = {
-            "Name": Name,
-            "Contact Number": Contact_Number,
-            "Email Address": Email_address
-            }
+        data = {
+        "Name": Name,
+         "Contact Number": Contact_Number,
+         "Email Address": Email_address
+          }
 
         # Write the data to a CSV file
-            with open(csv_path, mode='a', newline='') as file:
-                writer = csv.DictWriter(file, fieldnames=data.keys())
-                if file.tell() == 0:
-                    writer.writeheader()  # Write the header if the file is empty
-                writer.writerow(data)
+         with open(csv_path, mode='a', newline='') as file:
+         writer = csv.DictWriter(file, fieldnames=data.keys())
+         if file.tell() == 0:
+              writer.writeheader()  # Write the header if the file is empty
+         writer.writerow(data)
 
-            st.sidebar.success("Thanks for submitting your information!")
+         st.sidebar.success("Thanks for submitting your information!")
